c4aebb6bd75d7bb91b7ee987c6987bc8b2cc9605
dinuk2000 <dinuk_r@epiclanka.net>
2024-08-15 23:42:15 +0530

    intialize two services

---
+++ product-service/src/test/java/com/buyOnegetOne/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyOnegetOne.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
